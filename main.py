1401c4c25de6227ebb05b3feaefd9b5091a4bdfc
automatic-eureka <automatic-eureka@outlook.com>
2025-03-18 02:23:41 -0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def get_random_quote():
-    response = requests.get("https://api.quotable.io/random", verify=False)
-    data = response.json()
-    return f"{data['content']} - {data['author']}"
-
-print(get_random_quote())
